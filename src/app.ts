4832968f60364c3ac9d04c9e4690cdc2ce87f259
hschmiedhofer <h.schmiedhofer@gmail.com>
2023-08-28 15:06:08 +0200

    snapshot.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,74 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import {
+    Engine,
+    Scene,
+    ArcRotateCamera,
+    Vector3,
+    HemisphericLight,
+    Mesh,
+    MeshBuilder,
+    WebXRDefaultExperienceOptions,
+    WebXRDefaultExperience,
+    TransformNode,
+    Axis,
+    Quaternion,
+    SceneLoader,
+    Space,
+    WebXRFeatureName,
+    WebXRImageTracking,
+} from "@babylonjs/core";
 
+class Demo {
+    static async SetupXR(scene: Scene, options: WebXRDefaultExperienceOptions): Promise<WebXRDefaultExperience> {
+        scene.createDefaultEnvironment({ createGround: false, createSkybox: false });
+
+        const root = new TransformNode("root", scene);
+        // root.setEnabled(false);
+
+        const model = await SceneLoader.ImportMeshAsync(
+            "",
+            "https://piratejc.github.io/assets/",
+            "valkyrie_mesh.glb",
+            scene
+        );
+        model.meshes[0].parent = root;
+        root.rotationQuaternion = new Quaternion();
+
+        const xr = await scene.createDefaultXRExperienceAsync(options);
+
+        const featuresManager = xr.baseExperience.featuresManager;
+        const imageTracking = featuresManager.enableFeature(WebXRFeatureName.IMAGE_TRACKING, "latest", {
+            images: [
+                {
+                    src: "https://cdn.babylonjs.com/imageTracking.png",
+                    estimatedRealWorldWidth: 0.2,
+                },
+            ],
+        }) as WebXRImageTracking;
+
+        imageTracking.onTrackedImageUpdatedObservable.add((image) => {
+            // root.setPreTransformMatrix(image.transformationMatrix);
+            image.transformationMatrix.decompose(root.scaling, root.rotationQuaternion, root.position);
+            root.setEnabled(true);
+            root.translate(Axis.Y, 0.1, Space.LOCAL);
+        });
+
+        return xr;
+    }
+}
+
+// class Playground {
+//     public static async CreateScene(engine: Engine, canvas: HTMLCanvasElement): Promise<Scene> {
+//         const scene = new Scene(engine);
+//         await Demo.SetupXR(scene, {
+//             uiOptions: {
+//                 sessionMode: "immersive-ar",
+//             },
+//         });
+//         return scene;
+//     }
+// }
 class App {
     constructor() {
         // create the canvas html element and attach it to the webpage
