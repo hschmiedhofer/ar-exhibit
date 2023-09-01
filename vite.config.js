2bb0f148952915a68df81d5869ba262485916b45
hschmiedhofer <h.schmiedhofer@gmail.com>
2023-09-01 13:20:46 +0200

    expose npm run dev network via vite config, not npm script anymore.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ar-exhibit/'
+    base: '/ar-exhibit/',
+    server: {
+        port: 3443,
+        https: false,
+        // Uncomment to allow access from network
+        // (or use `npm run dev -- -- host=0.0.0.0`)
+        host: '0.0.0.0',
+    }
 });
