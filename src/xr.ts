31bbeecdea20e6426031a840d3209cf6832a014d
hschmiedhofer <h.schmiedhofer@gmail.com>
2023-09-04 10:27:49 +0200

    add some comments.

--- src/xr.ts
+++ src/xr.ts
@@ -27,19 +27,22 @@ export async function setupXR(
     const root = new TransformNode("root", scene);
     root.setEnabled(true);
 
+    //* load meshes from file and put them on common parent
     const model = await SceneLoader.ImportMeshAsync("", "", filenameGlb, scene);
-
     model.meshes.forEach((m) => {
         if (m.name === "frame") m.parent = root;
         if (m.name === "mirror") m.parent = root;
         if (m.name === "painting") m.dispose(true, true);
     });
-
     root.rotationQuaternion = new Quaternion();
 
-    const xr = await scene.createDefaultXRExperienceAsync(options);
+    //* create default experience helper
+    const defaultXrExperienceHelper = await scene.createDefaultXRExperienceAsync(options);
 
-    const featuresManager = xr.baseExperience.featuresManager;
+    //* create feature manager
+    const featuresManager = defaultXrExperienceHelper.baseExperience.featuresManager;
+
+    //# add and setup image tracking
     const imageTracking = featuresManager.enableFeature(WebXRFeatureName.IMAGE_TRACKING, "latest", {
         images: [
             {
@@ -48,7 +51,6 @@ export async function setupXR(
             },
         ],
     }) as WebXRImageTracking;
-
     imageTracking.onTrackedImageUpdatedObservable.add((image) => {
         // root.setPreTransformMatrix(image.transformationMatrix);
         image.transformationMatrix.decompose(root.scaling, root.rotationQuaternion, root.position);
@@ -56,7 +58,8 @@ export async function setupXR(
         root.translate(Axis.Y, 0.1, Space.LOCAL);
     });
 
+    //# add DOM overlay
     featuresManager.enableFeature(WebXRDomOverlay, "latest", { element: ".dom-overlay-container" });
 
-    return xr;
+    return defaultXrExperienceHelper;
 }
