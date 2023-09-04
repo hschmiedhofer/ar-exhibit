6730573ddd51fc3a7ee959979e229377436206d5
hschmiedhofer <h.schmiedhofer@gmail.com>
2023-09-04 09:57:51 +0200

    add experimental button.

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,29 @@ async function start() {
     overlayDiv.style.borderRadius = "5px";
     document.body.appendChild(overlayDiv);
 
+    const swapButton = document.createElement("button");
+    swapButton.innerText = "Swap Model";
+    swapButton.style.position = "fixed";
+    swapButton.style.top = "10px";
+    swapButton.style.right = "10px";
+    swapButton.style.width = "150px"; // Set width of the button
+    swapButton.style.height = "50px"; // Set height of the button
+    swapButton.style.fontSize = "18px"; // Set font size for the button's text
+    swapButton.style.backgroundColor = "#4CAF50"; // Set background color
+    swapButton.style.color = "white"; // Set text color
+    swapButton.style.border = "none"; // Remove border
+    swapButton.style.cursor = "pointer"; // Change cursor to pointer on hover
+    swapButton.style.borderRadius = "12px"; // Round the button corners
+    swapButton.addEventListener("mouseover", function () {
+        // Change background color on hover
+        swapButton.style.backgroundColor = "#45a049";
+    });
+    swapButton.addEventListener("mouseout", function () {
+        // Revert background color when not hovering
+        swapButton.style.backgroundColor = "#4CAF50";
+    });
+    document.body.appendChild(swapButton);
+
     const engine = new Engine(canvas, true);
     const scene = new Scene(engine);
 
